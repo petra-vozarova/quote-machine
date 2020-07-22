8b334e5ce87c4574952e5095f60cddba9a2ddfc8
petra-vozarova <61162312+petra-vozarova@users.noreply.github.com>
2020-07-22 17:05:38 +0100

    Updates

---
+++ precache-manifest.3c72338633bfadb61b4c0f3652f7ca21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d73db8fb3d17054de13955feaa9a6c2d",
+    "url": "/quote-machine/index.html"
+  },
+  {
+    "revision": "ce5d1e17d88dc5197b4e",
+    "url": "/quote-machine/static/css/main.c914bca4.chunk.css"
+  },
+  {
+    "revision": "d2278a7bea18fbc316bd",
+    "url": "/quote-machine/static/js/2.cea40203.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/quote-machine/static/js/2.cea40203.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce5d1e17d88dc5197b4e",
+    "url": "/quote-machine/static/js/main.d21b08a3.chunk.js"
+  },
+  {
+    "revision": "256db52c5a0c9de87c09",
+    "url": "/quote-machine/static/js/runtime-main.6ade76f3.js"
+  }
+]);
